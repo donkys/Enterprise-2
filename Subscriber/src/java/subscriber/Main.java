7abe3d64cdd18b5900fe38e2c1aeb3ccbf855f61
donkys <63050156@kmitl.ac.th>
2024-01-14 16:56:51 +0700

    ex2

---
+++ Subscriber/src/java/subscriber/Main.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package subscriber;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+/**
+ *
+ * @author donky
+ */
+
+public class Main {
+    @Resource(mappedName = "jms/SimpleJMSTopic")
+    private static Topic topic;
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    
+    public static void main(String[] args) {
+        Connection connection = null;
+        Session session = null;
+        Destination dest = (Destination) topic;
+        MessageConsumer consumer = null;
+        TextListener listener = null;
+        InputStreamReader inputStreamReader = null;
+        char answer = '\0';
+        
+        try {
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            consumer = session.createConsumer(dest);
+            listener = new TextListener();
+            consumer.setMessageListener(listener);
+            connection.start();
+            System.out.println(
+                    "To end program, type Q or q, " + "then <return>");
+            inputStreamReader = new InputStreamReader(System.in);
+
+            while (!((answer == 'q') || (answer == 'Q'))) {
+                try {
+                    answer = (char) inputStreamReader.read();
+                } catch (IOException e) {
+                    System.err.println("I/O exception: " + e.toString());
+                }
+            }
+        } catch (JMSException e) {
+            System.err.println("Exception occurred: " + e.toString());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                }
+            }
+        }
+    }
+
+    
+    
+}
